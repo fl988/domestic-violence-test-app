c01257f0643f32273665e829422d37d0d577b00e
febb <fl988@uowmail.edu.au>
2020-03-15 00:49:11 +1100

    this is not an empty message anymore okay

--- App.js
+++ App.js
@@ -244,9 +244,11 @@ Git Hub Command Line
   Modified Files
   - 'git status' to see whats up
   - 'git commit -am "your message"' to bundle your modified changes ready to be pushed into your github repository.
+  - 'git push origin master' to push it to the master file
 
   New Files and Folders
   - 'git status' to see the list of folders and files
-  - 'git add <file OR folder>' to add it in to your local repository officially
+  - 'git add <file OR folder>' to add it in to your local repository officially. Sample: git add components/CustomModal.js
   - 'git commit -am "your message" to bundle your new file/folder and be ready to be pushed into your githb  repository.
+  - 'git push origin master' to push it to the master file
 */
